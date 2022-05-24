d4b0266f2431c31078072e388b65092f726d4ad2
Nils Tijtgat <nilstijtgat@gmail.com>
2022-05-24 15:06:54 +0200

    Run Prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,58 @@
-import { AgmCoreModule } from "@agm/core";
-import { MatGoogleMapsAutocompleteModule } from "@angular-material-extensions/google-maps-autocomplete";
-import { CommonModule } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatButtonToggleModule } from "@angular/material/button-toggle";
-import { MatCardModule } from "@angular/material/card";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatInputModule } from "@angular/material/input";
-import { MatRadioModule } from "@angular/material/radio";
-import { MatTabsModule } from "@angular/material/tabs";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { RouterModule } from "@angular/router";
-import { Angulartics2Module } from "angulartics2";
-import { MarkdownModule } from "ngx-markdown";
-import { ConfigComponent } from "../config/config.component";
-import { AppComponent } from "./app.component";
+import { AgmCoreModule } from '@agm/core';
+import { MatGoogleMapsAutocompleteModule } from '@angular-material-extensions/google-maps-autocomplete';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { Angulartics2Module } from 'angulartics2';
+import { MarkdownModule } from 'ngx-markdown';
+import { ConfigComponent } from '../config/config.component';
+import { AppComponent } from './app.component';
 
 const googleMapsParams = {
-  apiKey: "AIzaSyD7aI7elBUMplf9aOmT-wMhiojOxBj5Jxg",
-  libraries: ["places"],
-  language: "en",
-  // region: 'DE'
+    apiKey: 'apiKey',
+    libraries: ['places'],
+    language: 'en',
+    // region: 'DE'
 };
 
 @NgModule({
-  declarations: [AppComponent, ConfigComponent],
-  imports: [
-    CommonModule,
-    BrowserModule.withServerTransition({ appId: "serverApp" }),
-    BrowserAnimationsModule,
-    RouterModule.forRoot([], {
-      initialNavigation: "enabled",
-      relativeLinkResolution: "legacy",
-    }),
-    Angulartics2Module.forRoot(),
-    MarkdownModule.forRoot(),
-    AgmCoreModule.forRoot(googleMapsParams),
-    MatGoogleMapsAutocompleteModule,
-    FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatInputModule,
-    MatExpansionModule,
-    MatTabsModule,
-    MatRadioModule,
-  ],
-  providers: [],
-  schemas: [NO_ERRORS_SCHEMA],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, ConfigComponent],
+    imports: [
+        CommonModule,
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        BrowserAnimationsModule,
+        RouterModule.forRoot([], {
+            initialNavigation: 'enabled',
+            relativeLinkResolution: 'legacy',
+        }),
+        Angulartics2Module.forRoot(),
+        MarkdownModule.forRoot(),
+        AgmCoreModule.forRoot(googleMapsParams),
+        MatGoogleMapsAutocompleteModule,
+        FlexLayoutModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatInputModule,
+        MatExpansionModule,
+        MatTabsModule,
+        MatRadioModule,
+    ],
+    providers: [],
+    schemas: [NO_ERRORS_SCHEMA],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
